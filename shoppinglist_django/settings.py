50e25470153f184ec1b191772fef844979c97a4e
suvarna <suvarna0318@gmail.com>
2022-06-29 20:58:32 +0530

    changes in settings

--- shoppinglist_django/settings.py
+++ shoppinglist_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'df08yip207sh(r&$auw@wazcwq!da1rdvyavl(*kulqpyj$cb6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['exam-dep.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
